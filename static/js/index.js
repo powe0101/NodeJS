dac109d7f8b0b051da748e73a8bf469df0453853
Hyunjin Gil <hyunjin.direct@gmail.com>
2020-03-24 15:43:22 +0900

    코드 리팩토링

--- static/js/index.js
+++ static/js/index.js
@@ -23,73 +23,73 @@ socket.on('update', function(data) {
   var className = ''
 
   // 타입에 따라 적용할 클래스를 다르게 지정
-  switch(data.type) {
+  switch(data.type)
+  {
     //그리기 영역
     case 'draw':
       className = 'draw'
-      var cnvs = document.getElementById('cnvs');
-      // cavnas 있음
-      if (cnvs.getContext)
-      {
-          // 캔버스 컨텍스트
-          var ctx = cnvs.getContext('2d');
-          ctx.fillStyle = data.message[0];
-          ctx.fillRect(data.message[1], data.message[2], data.message[3], data.message[4]);
-      }
-      break
-
+      DrawPoint(data)
+      return
     //채팅 영역
     case 'message':
       className = 'other'
-      AppendMessage(className, data)
       break
     case 'connect':
       className = 'connect'
-      AppendMessage(className, data)
       break
     case 'disconnect':
       className = 'disconnect'
-      AppendMessage(className, data)
       break
   }
+  AppendMessage(className, data)
 })
+
+function DrawPoint(data) {
+  var cnvs = document.getElementById('cnvs')
+  // cavnas 있음
+  if (cnvs.getContext) {
+    // 캔버스 컨텍스트
+    var ctx = cnvs.getContext('2d')
+    ctx.fillStyle = data.message[0]
+    ctx.fillRect(data.message[1], data.message[2], data.message[3], data.message[4])
+  }
+}
 
 function AppendMessage(className, data)
 {
-  var chat = document.getElementById('chat')
-
   var message = document.createElement('div')
-  var node = document.createTextNode(`${data.name}: ${data.message}`)
+  {
+    var node = document.createTextNode(`${data.name}: ${data.message}`)
+    message.classList.add(className)
+    message.appendChild(node)
+  }
 
-  message.classList.add(className)
-  message.appendChild(node)
-  chat.appendChild(message)
-  chat.scrollTop = chat.scrollHeight;
+  var chat = document.getElementById('chat')
+  {
+    chat.appendChild(message)
+    chat.scrollTop = chat.scrollHeight;
+  }
 }
 
 /* 메시지 전송 함수 */
 function send() {
   // 입력되어있는 데이터 가져오기
-  var message = document.getElementById('inputMessageBox').value
-  
-  // 가져왔으니 데이터 빈칸으로 변경
-  document.getElementById('inputMessageBox').value = ''
-
+  var message = GetMessageAndClearInputBox()
   // 내가 전송할 메시지 클라이언트에게 표시
-  var chat = document.getElementById('chat')
-  var msg = document.createElement('div')
-  var node = document.createTextNode(`${name}: ${message}`)
-  msg.classList.add('me')
-  msg.appendChild(node)
-  chat.appendChild(msg)
-  chat.scrollTop = chat.scrollHeight;
+  AppendMessage('me',{name : name, message : message})
   // 서버로 message 이벤트 전달 + 데이터와 함께
   socket.emit('message', {type: 'message', message: message})
 }
 
+function GetMessageAndClearInputBox() {
+  var message = document.getElementById('inputMessageBox').value
+  // 가져왔으니 데이터 빈칸으로 변경
+  document.getElementById('inputMessageBox').value = ''
+  return message
+}
+
 function enterkey() {
     if (window.event.keyCode == 13) {
-
          // 엔터키가 눌렸을 때 실행할 내용
          send();
     }
